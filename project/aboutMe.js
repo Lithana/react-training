3809752824538f5eca634ad2cdc1c63d802f523b
Lithana <150119704+Lithana@users.noreply.github.com>
2024-10-12 19:50:43 +0530

    aboutMe.js

---
+++ project/aboutMe.js
@@ -0,0 +1,33 @@
+.about-me-container {
+    margin: 20px; 
+  }
+  
+  .link-label {
+    font-weight: bold; 
+    color: #e32d1c; 
+  }
+  
+  .link {
+    font-weight: bold; 
+    color: #009688; 
+    text-decoration: none; 
+  }
+  
+  .link:hover {
+    text-decoration: underline; 
+  }
+  
+
+  .about-me-textarea {
+    width: 100%; 
+    margin-top: 10px;
+  }
+  
+  .social-links {
+    margin: 10px 0; 
+  }
+  
+  .social-links label {
+    display: block; 
+    margin-bottom: 10px; 
+  }
